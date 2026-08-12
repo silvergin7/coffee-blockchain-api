e6f0b0dc50428f7fb10082f10f6ddb989c514f84
silvergin7 <timbjornhov@gmail.com>
2026-08-12 20:09:33 +0200

    Feat: green - POST /transactions

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ const { Blockchain } = require('./blockchain.js');
 const app = express();
 const blockchain = new Blockchain();
 
+app.locals.blockchain = blockchain;
+app.use(express.json());
+
 app.get('/blockchain', (req, res) => {
   res.status(200).json(blockchain.chain);
+});
+
+app.post('/transactions', (req, res) => {
+  blockchain.addTransaction(req.body);
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
