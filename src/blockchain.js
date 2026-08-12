2cfddef63e5d1f5079e45b7b90083d1feaf11087
silvergin7 <timbjornhov@gmail.com>
2026-08-12 19:03:27 +0200

    Feat: green - mineBlock (+ createGenesisBlock)

--- src/blockchain.js
+++ src/blockchain.js
@@ -10,4 +10,58 @@ function calculateHash(block) {
   return crypto.createHash('sha256').update(payload).digest('hex');
 }
 
-module.exports = { calculateHash };
+class Blockchain {
+  constructor() {
+    this.difficulty =
+      process.env.NODE_ENV === 'test'
+        ? 1
+        : Number(process.env.POW_DIFFICULTY ?? 3);
+    this.pendingTransactions = [];
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    const genesisBlock = {
+      index: 0,
+      timestamp: 0,
+      transactions: [],
+      previousHash: '0',
+      nonce: 0,
+    };
+
+    genesisBlock.hash = calculateHash(genesisBlock);
+    return genesisBlock;
+  }
+
+  mineBlock() {
+    const transactions = [...this.pendingTransactions];
+
+    const previousHash = this.chain.at(-1).hash;
+    const index = this.chain.length;
+    const timestamp = Date.now();
+    const target = '0'.repeat(this.difficulty);
+
+    let nonce = 0;
+    let hash = calculateHash({ index, previousHash, transactions, nonce });
+
+    while (!hash.startsWith(target)) {
+      nonce++;
+      hash = calculateHash({ index, previousHash, transactions, nonce });
+    }
+
+    const block = {
+      index,
+      timestamp,
+      transactions,
+      previousHash,
+      nonce,
+      hash,
+    };
+
+    this.chain.push(block);
+    this.pendingTransactions = [];
+    return block;
+  }
+}
+
+module.exports = { calculateHash, Blockchain };
